77cf6b1588896d394fbb20346cb7ec366f241ef1
Roberto Mauceri <rob_mau@hotmail.it>
2024-03-04 17:17:20 +0100

    translation

--- cylindrical_perspective.m
+++ cylindrical_perspective.m
@@ -1,76 +1,74 @@
 % ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-%  Generatore Griglia per Prospettiva Cilindrica (Disegno)
+%  Grid Generator for Cylindrical Perspective (Drawing)
 % ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-clear all
-close all
+clear all; 
+close all; 
+clc;
 
-% parametri griglia cubica
-% la griglia cubica è costituita da 3 famiglie di segmenti tra loro
-% paralleli ed equidistanziati (distanza di 1 "unità"). Le famiglie sono
-% tra loro mutuamente ortogonali. Ogni segmento è rappresentato con
-% "num_points" punti.
-half_len = 2;       % semi-lunghezza del lato del cubo ("unità" intere!)
-num_points = 400;   % numero di punti in ciascun segmento
+% Cubic Grid Parameters
+% The cubic grid consists of 3 sets of mutually parallel and equidistant 
+% segments (spaced by 1 "unit"). The sets are mutually orthogonal. 
+% Each segment is represented by "num_points" points.
+half_len = 2;       % half-length of the cube side (integer number)
+num_points = 400;   % number of points in each segment
 
-% parametri geometrici
-% posizione del centro della griglia cubica
+% Geometric Parameters
+% position of the center of the cubic grid
 d = [0; 0; 0];  
-% rotazione che descrive l'orientamento della griglia (param. asse-angolo)
-n = [1; 1; 1];  % asse della rotazione 
-angle = pi/8; 	% angolo della rotazione
-% raggio della superficie cilindrica
+% rotation describing the orientation of the grid (axis-angle parameters)
+n = [1; 1; 1];  % rotation axis
+angle = pi/8; 	% rotation angle
+% radius of the cylindrical surface
 radius = 1; 
 
-% matrice di trasformazione T tra due sistemi di riferimento genericamente
-% orientati (matrice omogenea)
+% transformation matrix T between two arbitrarily oriented frames
+% (homogeneous matrix)
 n = n / norm(n);
+S = skew(n);
 I = eye(3);
-A = n * n.';    % proiettore ortogonale sullo span{n}
-N = skew(n);   % matrice antisimmetrica generata da n 
-% formula di Rodrigues
-R = A + (I - A) * cos(angle) + N * sin(angle);
-% matrice omogenea che codifica la roto-traslazione
+% Rodrigues' rotation formula
+R = I + sin(angle) * S + (1 - cos(angle)) * S^2;
+% homogeneous matrix encoding the rotation and translation
 T = [R, d; [0, 0, 0, 1]];
 
 % ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-%  Inizializzazione Matrici
+%  Matrix Initialization
 % ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-% le lettere x, y, z individuano ciascuna delle 3 famiglie di 
-% segmenti mutuamente ortogonali che compongono la griglia cubica
-% (allineate, rispettivamente, lungo gli assi x, y e z)
-
+% The letters x, y, z identify each of the 3 families of segments 
+% that are mutually orthogonal composing the cubic grid 
+% (aligned along the x, y, and z axes, respectively)
 max_index  = 2 * half_len + 1;
 
-% famiglie di segmenti allineate con gli assi coordinati (pedice a)
-% inizializzazione
+% families of segments aligned with the coordinate axes (subscript a)
+% initialization
 px_a = zeros(max_index, max_index, 4, num_points);
 py_a = zeros(max_index, max_index, 4, num_points);
 pz_a = zeros(max_index, max_index, 4, num_points);
 
-% famiglie di segmenti roto-traslate tramite la trasform. T (pedice b)
-% inizializzazione
+% families of segments transformed through T transformation (subscript b)
+% initialization
 px_b = zeros(max_index, max_index, 4, num_points);
 py_b = zeros(max_index, max_index, 4, num_points);
 pz_b = zeros(max_index, max_index, 4, num_points);
 
-% famiglie di segmenti proiettate sul cilindro --> diventano tre famiglie
-% di curve, in particolare archi di ellissi (pedice c)
-% inizializzazione
+% families of segments projected onto the cylinder -- > they become three 
+% families of curves, specifically arcs of ellipses. (subscript c)
+% initialization
 px_c = zeros(max_index, max_index, 3, num_points);
 py_c = zeros(max_index, max_index, 3, num_points);
 pz_c = zeros(max_index, max_index, 3, num_points);
 
-% famiglie di curve sul cilindro "srotolato" --> gli archi di ellissi
-% diventano archi di sinusoidi (pedice d)
-% inizializzazione
+% families of curves on the 'unrolled' cylinder --> the arcs of ellipses 
+% become arcs of sinusoids. (subscript d)
+% initialization
 px_d = zeros(max_index, max_index, 2, num_points);
 py_d = zeros(max_index, max_index, 2, num_points);
 pz_d = zeros(max_index, max_index, 2, num_points);
 
 % ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-%  Calcolo Matrici
+%  Matrix Computation
 % ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-% famiglie di segmenti allineate con gli assi coordinati (pedice a)
+% families of segments aligned with the coordinate axes (subscript a)
 step = 2 * half_len / (num_points - 1);
 
 x = -half_len:step:half_len;
@@ -79,7 +77,6 @@ z = -half_len:step:half_len;
 
 for i = 1:max_index
     for j = 1:max_index
-        
         px_a(i,j,:,:) = [x; 
                          ones(1, num_points) * ((i - 1) - half_len); 
                          ones(1, num_points) * ((j - 1) - half_len); 
@@ -97,21 +94,19 @@ for i = 1:max_index
     end
 end
      
-% famiglie di segmenti roto-traslate tramite la trasform. T (pedice b)
+% families of segments transformed through T transformation (subscript b)
 for i = 1:max_index
     for j = 1:max_index
-        
         px_b(i,j,:,:) = T * squeeze(px_a(i,j,:,:));
         py_b(i,j,:,:) = T * squeeze(py_a(i,j,:,:));
         pz_b(i,j,:,:) = T * squeeze(pz_a(i,j,:,:));
     end
 end
 
-% famiglie di segmenti proiettate sul cilindro --> diventano tre famiglie
-% di curve, in particolare archi di ellissi (pedice c)
+% families of segments projected onto the cylinder -- > they become three 
+% families of curves, specifically arcs of ellipses. (subscript c)
 for i = 1:max_index
     for j = 1:max_index
-        
         px_c(i,j,:,:) = radius/sqrt(px_b(i,j,1,:).^2 + ...
                         px_b(i,j,2,:).^2) .* px_b(i,j,1:3,:);
         py_c(i,j,:,:) = radius/sqrt(py_b(i,j,1,:).^2 + ...
@@ -121,11 +116,10 @@ for i = 1:max_index
     end
 end
 
-% famiglie di curve sul cilindro "srotolato" --> gli archi di ellissi
-% diventano archi di sinusoidi (pedice d)
+% families of curves on the 'unrolled' cylinder --> the arcs of ellipses 
+% become arcs of sinusoids. (subscript d)
 for i = 1:max_index
     for j = 1:max_index
-        
         px_d(i,j,1,:) =  radius * atan2(px_c(i,j,2,:), px_c(i,j,1,:));
         px_d(i,j,2,:) =  px_c(i,j,3,:);
         
@@ -138,14 +132,14 @@ for i = 1:max_index
 end
 
 % ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-%  Figura 1 - Griglia Cubica Tridimensionale & Cilindro
+%  Figure 1 - 3D Cubic Grid & Cylinder
 % ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-f1 = figure('Name', 'Cilindro', 'Color', 'w');
+f1 = figure('Name', '3D Cubic Grid & Cylinder Projection', 'Color', 'w');
 axis vis3d equal off
 axis([-1, 1, -1, 1, -1, 1] * 10);
 hold on
 
-% superficie cilindrica
+% cylindrical surface
 [t, h] = meshgrid(0:pi/10:2*pi, -8:0.5:8);
 xc = radius .* cos(t);
 yc = radius .* sin(t);
@@ -153,13 +147,13 @@ zc = h;
 surf(xc, yc, zc, 'FaceColor', 'k', 'EdgeColor', 'none')
 alpha 0.05
 
-% centro del sistema di riferimento e centro della griglia cubica
+% center of the reference system and center of the cubic grid
 plot3(0, 0, 0, '*')
 plot3(d(1), d(2), d(3), '*')
 
 for i = 1:max_index
     for j = 1:max_index
-        % segmenti griglia cubica
+        % cubic grid segments
         plot3(squeeze(px_b(i,j,1,:)), ...
               squeeze(px_b(i,j,2,:)), ...
               squeeze(px_b(i,j,3,:)), 'Color', 'b')
@@ -170,7 +164,7 @@ for i = 1:max_index
               squeeze(pz_b(i,j,2,:)), ...
               squeeze(pz_b(i,j,3,:)), 'Color', 'm')
         
-        % proiezioni sulla superf. cilindrica (archi di ellisse)
+        % projections onto the cylindrical surface (arcs of ellipses)
         plot3(squeeze(px_c(i,j,1,:)), ...
               squeeze(px_c(i,j,2,:)), ...
               squeeze(px_c(i,j,3,:)), 'Color', 'b')
@@ -184,17 +178,16 @@ for i = 1:max_index
 end
 
 % ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-%  Figura 2 - Griglia Prospettica Bidimensionale
+%  Figura 2 - 2D Perspective Grid
 % ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-f2 = figure('Name', 'Proiezione', 'Color', 'w');
+f2 = figure('Name', '2D Projection', 'Color', 'w');
 axis equal
 axis([-pi, pi, -pi/2, pi/2]);
 hold on
 
-% tracciamento curve della griglia prospettica 
+% drawing curves of the perspective grid
 for i = 1:max_index
     for j = 1:max_index
-        
         plot(-squeeze(pz_d(i,j,1,:)), squeeze(pz_d(i,j,2,:)), ...
              '.', 'MarkerSize', 1, 'Color', 'b')
         plot(-squeeze(py_d(i,j,1,:)), squeeze(py_d(i,j,2,:)), ...
@@ -204,7 +197,7 @@ for i = 1:max_index
     end
 end
 
-% tracciamento dei riferimenti
+% drawing reference frames
 plot(-squeeze(px_d(1,1,1,:)), squeeze(px_d(1,1,2,:)), ...
      '.', 'MarkerSize', 3, 'Color', 'k')
 plot(-squeeze(px_d(max_index,max_index,1,:)), ...
@@ -224,11 +217,11 @@ plot(-squeeze(pz_d(max_index,max_index,1,:)), ...
      '.', 'MarkerSize', 3, 'Color', 'k')
 
 % ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-%  Definizione Funzioni
+%  Function Definitions
 % ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-% Funzione out = skew(in):
-%   in:  vettore tridimensionale
-%   out: matrice antisimmetrica 3*3
+% Funzion out = skew(in):
+%   in:  3D vector
+%   out: 3*3 skew-symmetric matrix
 function out = skew(in)
     a = in(1);
     b = in(2);
